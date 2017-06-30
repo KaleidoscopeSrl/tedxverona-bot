6b47f65164588b456bd52e52ee6856bb7fc6e678
Filippo Matteo Riggio <filippo@kaleidoscope.it>
2017-06-30 14:46:58 +0200

    work in progress

--- start_thread.js
+++ start_thread.js
@@ -35,7 +35,7 @@ function setupMenu()
 		setting_type: "persistent_menu",
 		persistent_menu: [{
 			locale: 'default',
-			composer_input_disabled: true,
+			composer_input_disabled: false,
 			call_to_actions:[{
 				title: 'TEDxVerona 2017',
 				type: 'nested',
